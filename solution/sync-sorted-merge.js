8c949d51fed97304b044e9f7e2901c6d843a04e0
PrabdeepPannu <25015271+PrabdeepPannu@users.noreply.github.com>
2024-04-19 09:25:46 -0400

    [sync-sorted-solution] Utilize PQ and print the records using printer

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,35 @@
+/**
+ * Implement Priority Queue for the sorting and fetching the records
+ * Sync add those records to the queue and print the logs till the queue is Empty
+ * @params
+ * logSource - array of log sources
+ * printer - printer instance
+ */
 "use strict";
+const PriorityQueue = require("../lib/priority-queue");
 
-// Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  // Initialize a priority queue
+  const queue = new PriorityQueue();
+  
+  // load the queue with records
+  logSources.forEach((logSource, index) => {
+    const log = logSource.pop();
+    if (log) queue.add({ ...log, sourceIndex: index });
+  });
+
+  // loop till queue is not empty
+  while (!queue.isEmpty()) {
+    // get the oldest record
+    const { date, msg, sourceIndex } = queue.pop();
+    printer.print({date, msg});
+    const nextLogEntry = logSources[sourceIndex].pop();
+    // if there are more records 
+    if (nextLogEntry) {
+      queue.add({ ...nextLogEntry, sourceIndex });
+    }
+  }
+  // exit printer
+  printer.done();
 };
